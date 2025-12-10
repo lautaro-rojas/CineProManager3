08c7ee47a614a1a30d4cd18f78e721a7c58a1fef
Lautaro Rojas <lautaro.rojas02@gmail.com>
2025-12-10 12:46:44 -0300

    Added Movie entity

---
+++ Entities/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Entities
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [MaxLength(100)] 
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Genre is required")]
+        [MaxLength(100)]
+        public string Genre { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Duration is required")]
+        public int Duration { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public byte[] Poster { get; set; } = Array.Empty<byte>();
+    }
+}
